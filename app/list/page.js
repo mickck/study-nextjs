b88b4d27b87640094f87831cc236a090e2f1d883
Mickck <mikemwkim@gmail.com>
2023-04-17 23:53:53 +1000

    first commit and built header and footer

---
+++ app/list/page.js
@@ -0,0 +1,14 @@
+export default function List() {
+  return (
+    <div>
+      <h4 className="">List of the products</h4>
+      <div className="food">
+        <h4>Product 1 $40</h4>
+      </div>
+
+      <div className="food">
+        <h4>Product 2 $48</h4>
+      </div>
+    </div>
+  );
+}
