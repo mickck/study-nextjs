4e609bbf23ecfc0e844ca5604f737fa728e09539
Mickck <mikemwkim@gmail.com>
2023-04-25 17:54:46 +1000

    finised first section

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,10 @@
+"use client";
+import { useState } from "react";
+
 export default function List() {
   let products = ["Tomato", "Pasta", "Coconut"];
+  const [count, setCount] = useState([0, 0, 0]);
+
   return (
     <div>
       <h3 className="">List of the products</h3>
@@ -7,6 +12,27 @@ export default function List() {
         <div className="food" key={i}>
           <img src={`/food${i}.png`} />
           <h4>{product} $40</h4>
+          <span>total: ${count[i]}</span>
+          <button
+            onClick={() => {
+              let copy = [...count];
+              copy[i]++;
+              setCount(copy);
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              if (count[i] > 0) {
+                let copy = [...count];
+                copy[i]--;
+                setCount(copy);
+              }
+            }}
+          >
+            -
+          </button>
         </div>
       ))}
     </div>
